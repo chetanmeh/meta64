7ff1f7911bc35f5d618685dbdf571fbd4b79e94f
Clay Ferguson <wclayferg@gmail.com>
2015-07-22 10:33:58 -0500

    refactored pageBuilders array init

--- src/main/resources/public/js/meta64/pg/pageMgr.js
+++ src/main/resources/public/js/meta64/pg/pageMgr.js
@@ -2,17 +2,18 @@ console.log("running module: pageMgr.js");
 
 var pageMgr = function() {
 
-	var _ = {
 	/*
-		 * Contains a mapping of all page names and the functions to call to
-		 * generate that page. Each page initializer will usually only be run
-		 * one time to generate the HTML.
-		 */
-		pageBuilders : [],
-		
+	 * Contains a mapping of all page names and the functions to call to
+	 * generate that page. Each page initializer will usually only be run one
+	 * time to generate the HTML.
+	 */
+	var pageBuilders = null;
+
+	var _ = {
+
 		buildPage : function(pageName) {
-			for (var i = 0; i < _.pageBuilders.length; i++) {
-				var builder = _.pageBuilders[i];
+			for (var i = 0; i < pageBuilders.length; i++) {
+				var builder = pageBuilders[i];
 				// console.log("Checking page builder: "+builderObj.name);
 				if (pageName.contains(builder.name)) {
 					// console.log("found page builder.");
@@ -21,7 +22,7 @@ var pageMgr = function() {
 						builder.build();
 						builder.built = true;
 					}
-					
+
 					if (builder.init) {
 						builder.init();
 					}
@@ -29,68 +30,55 @@ var pageMgr = function() {
 				}
 			}
 		},
-		
+
 		initializePageBuilders : function() {
-			if (_.pageBuilders.length > 0) {
+			if (pageBuilders) {
 				console.log("initializePageBuilders called twice ?");
 				return;
 			}
 
-			_.pageBuilders.push({
+			pageBuilders = [
+			{
 				name : "#signupPg",
 				build : signupPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#loginPg",
 				build : loginPg.build,
 				built : false,
 				init : loginPg.init
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#prefsPg",
 				build : prefsPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#changePasswordPg",
 				build : changePasswordPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#exportPg",
 				build : exportPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#importPg",
 				build : importPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#searchPg",
 				build : searchPg.build,
 				built : false,
 				init : null
-			});
-			
-			_.pageBuilders.push({
+			}, {
 				name : "#uploadPg",
 				build : uploadPg.build,
 				built : false,
 				init : uploadPg.init
-			});
+			} ];
 		}
 	};
 
